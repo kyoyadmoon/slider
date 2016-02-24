66325150cb2eb066e2fe6c120c87df5d4ba259cf
dmoon <dmoon.t@gmail.com>
2016-02-24 15:31:23 +0800

    example test

--- examples/marks.js
+++ examples/marks.js
@@ -4,13 +4,13 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const Slider = require('vertical-rc-slider');
 
-const style = {width: 400, height: 150, margin: 50};
+const style = {width: 400, height: 300, margin: 50};
 const marks = {
-  0: '0°C',
-  26: '26°C',
-  37: '37°C',
-  50: '50°C',
-  100: '100°C',
+  0: '0',
+  30: '30',
+  50: '50',
+  75: '75',
+  120: '120',
 };
 
 function log(value) {
@@ -20,7 +20,7 @@ function log(value) {
 ReactDOM.render(
   <div>
     <div style={style}>
-      <Slider min={0} marks={marks} step={10} onChange={log} defaultValue={20} />
+      <Slider max={120} marks={marks} step={10} onChange={log} defaultValue={20} />
     </div>
   </div>
   , document.getElementById('__react-content'));
